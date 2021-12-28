862576b2988ea0a7aa8bab955c91472911c3ddbe
Justin Elmore <jelmore1674@pm.me>
2021-12-27 21:38:54 -0600

    added contnet to member-detail and member card

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,25 @@
+import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+	selector: 'app-member-list',
+	templateUrl: './member-list.component.html',
+	styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
+	members: Member[];
+	constructor(private memberService: MembersService) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.loadMembers();
+	}
 
-  ngOnInit(): void {
-  }
-
+	loadMembers() {
+		return this.memberService
+			.getMembers()
+			.subscribe((members: Member[]) => {
+				this.members = members;
+			});
+	}
 }
